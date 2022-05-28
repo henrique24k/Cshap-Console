72f96004712f0dcf86f646b0629a65a3a87b8e39
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-05-28 15:25:59 -0300

    Add files via upload

---
+++ AtvWhile/AtvWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtvWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, quant = 0;
+
+            string resp = "S";
+
+            while (resp == "S" || resp == "s")
+            {
+                Console.WriteLine("Digite um valor: ");
+                n = int.Parse(Console.ReadLine());
+                quant++;
+
+                Console.WriteLine("Deseja digitar outro valor ? S/N");
+                resp = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Voce digitou {0} valores ", quant);
+
+            Console.ReadKey();
+        }
+    }
+}
