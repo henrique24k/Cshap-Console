9ef8871133a1c0a1620d450c40822af587a47f42
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-05-28 13:20:28 -0300

    Add files via upload

---
+++ tabuadaWhile/tabuadaWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuadaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num, mult, resultado;
+
+            Console.WriteLine("Digite um numero para mostrar na tabuada: ");
+            num = int.Parse(Console.ReadLine());
+
+            mult = 1;
+
+            while (mult <=100)
+            {
+                resultado = num * mult;
+                Console.WriteLine("{0} * {1} = {2}", num, mult, resultado);
+                mult++;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
