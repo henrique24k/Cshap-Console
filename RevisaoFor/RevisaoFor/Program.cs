865d7ff4679bd06044fa28d637e478583902c9fa
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-06-16 12:33:05 -0300

    Add files via upload

---
+++ RevisaoFor/RevisaoFor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisaoFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for(i=1; i<=150; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(i + " Multiplos de 10");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
