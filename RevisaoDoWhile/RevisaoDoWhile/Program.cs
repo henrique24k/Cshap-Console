24adcc932cf2ba2a509677f426f9d94a65a6e7a2
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-06-17 19:51:00 -0300

    Add files via upload

---
+++ RevisaoDoWhile/RevisaoDoWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisaoDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char resposta;
+            int impar = 0, par = 0, num;
+
+            do
+            {
+                Console.WriteLine("Digite um numero inteiro: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 1)
+                {
+                    impar++;
+                }
+                else
+                {
+                    par++;
+                }
+                Console.WriteLine("Dejesa Sair? S ou N: ");
+                resposta = char.Parse(Console.ReadLine());
+
+            } while (resposta != 'S');
+
+            Console.WriteLine("Voce digitou {0} impares e {1} pares ", impar, par);
+
+            Console.ReadKey();
+
+        }
+        
+
+    }
+}
