9b6a1897521d7dd75c884b29fd4abbf8192f0392
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-07-05 18:56:52 -0300

    Add files via upload

---
+++ Vetores01/Vetores01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vetores01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string[10];
+            int i;
+
+            for(i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite o {0} nome: ", i+1); 
+                nome[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0} nome: {1} ", i+1, nome[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
