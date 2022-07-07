058892d7cd337804eb0d5ce708455c9e282023b0
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-07-07 15:25:49 -0300

    Add files via upload

---
+++ atvVetor02/atvVetor02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace atvVetor02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double salario_min;
+            double[] salarios = new double[10];
+
+            Console.WriteLine("Digite o Salario minimo : ");
+            salario_min = double.Parse(Console.ReadLine());
+
+            //Atrbuição do calculo no vetor salarios
+            for (i = 0; i < salarios.Length; i++)
+                salarios[i] = (i + 1) * salario_min;
+
+            //Exibição dos dados calculados
+            for (i = 0; i < salarios.Length; i++)
+                Console.WriteLine("{0} Salario(s) Minimo(s) = {1}", (i + 1), salarios[i].ToString("#00.00"));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
