6a69fbeb4565da5ccb3b78907f991ac65e88c69e
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-07-08 17:44:49 -0300

    Add files via upload

---
+++ ExibirCaractereForn/ExibirCaractereForn/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExibirCaractereForn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            char a;
+
+            Console.WriteLine("Digite um caracter: ");
+            a = char.Parse(Console.ReadLine());
+
+            b = Convert.ToInt16(a);
+
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
