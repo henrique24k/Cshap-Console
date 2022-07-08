ced2941e8166f0a7b3c71989e22238e33e068962
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-07-08 17:44:16 -0300

    Add files via upload

---
+++ ExibindoCaracteres/ExibindoCaracteres/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExibindoCaracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for(i = 0; i <= 26; i++)
+            {
+                char a = Convert.ToChar(i);
+                Console.WriteLine(i+""+a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
