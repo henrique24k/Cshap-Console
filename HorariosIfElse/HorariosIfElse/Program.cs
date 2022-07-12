65021db8b7a9236f6c43fb45c080c2f96a2fee87
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-07-12 17:51:24 -0300

    Add files via upload

---
+++ HorariosIfElse/HorariosIfElse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HorariosIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora;
+
+            Console.WriteLine("Digite a hora: ");
+            hora = int.Parse(Console.ReadLine());
+
+            if (hora <= 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+
+            else if (hora > 19)
+            {
+                Console.WriteLine("Boa noite!");
+            }
+
+            else 
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
