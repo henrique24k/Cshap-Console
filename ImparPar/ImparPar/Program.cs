81dcc6099eff8587565c12e105284e49dced4d9d
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-07-12 17:50:07 -0300

    utilizando if e resto da divisão

---
+++ ImparPar/ImparPar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ImparPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Entre com um numero inteiro: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else 
+            {
+                Console.WriteLine("Impar");
+            }
+
+
+        }
+    }
+}
