11930102aa5aed6e5f541d534ad9386c18c85478
Henrique Vieira <72998582+henrique24k@users.noreply.github.com>
2022-07-19 09:50:46 -0300

    Add files via upload

---
+++ WhileUdy/WhileUdy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace WhileUdy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        double x;
+
+        Console.Write("Digite um número: ");
+        x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while(x >= 0.0) { 
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+            Console.Write("Digite outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        Console.WriteLine("Número negativo!");
+        Console.ReadKey();
+
+        }
+    }
+}
